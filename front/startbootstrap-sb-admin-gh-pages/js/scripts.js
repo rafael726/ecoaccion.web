888934d5c58cc50ad461d52a2a1627d43b42a9b6
lorenqr <lorenagr223@gmail.com>
2025-08-28 21:30:50 -0500

    Configuracion de la plantilla del fronted

--- front/startbootstrap-sb-admin-gh-pages/js/scripts.js
+++ front/startbootstrap-sb-admin-gh-pages/js/scripts.js
@@ -1,26 +1,19 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
-// Scripts
-// 
+// scripts.js
 
-window.addEventListener('DOMContentLoaded', event => {
+const user = JSON.parse(localStorage.getItem("user"));
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
+if (user) {
+  if (user.rol === "admin") {
+    const adminView = document.getElementById("admin-view");
+    const adminMenu = document.getElementById("admin-menu-item");
 
-});
+    if (adminView) adminView.style.display = "block";
+    if (adminMenu) adminMenu.style.display = "block";
+
+  } else if (user.rol === "user") {
+    const userView = document.getElementById("user-view");
+    if (userView) userView.style.display = "block";
+  }
+} else {
+  window.location.href = "login.html";
+}
